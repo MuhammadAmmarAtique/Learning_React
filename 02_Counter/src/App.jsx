b00c01aa62532745341ec635aade79fbb01545f1
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-02-06 14:21:43 +0500

    Created a Counter App using React Hook

---
+++ 02_Counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  let addValue = () => {
+    if (counter === 20) {
+      document.querySelector("#display").innerHTML =
+        "Cant increase value more then 20";
+    } 
+    else {
+      setCounter(counter = counter+1);
+  document.querySelector("#display").innerHTML ="";
+
+    }
+  };
+
+  let deleteValue = () => {
+    if (counter === 0) {
+      document.querySelector("#display").innerHTML =
+        "Cant decrease value less then 0";
+    } 
+    else {
+      setCounter(counter = counter-1);
+  document.querySelector("#display").innerHTML ="";
+
+    }
+  };
+
+  return (
+    <>
+      <h1>Using React Hooks in Simple Counter App </h1>
+      <h3>Counter value: {counter}</h3>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={deleteValue}>Remove value</button>
+      <p id="display"></p>
+    </>
+  );
+
+
+}
+
+export default App;
