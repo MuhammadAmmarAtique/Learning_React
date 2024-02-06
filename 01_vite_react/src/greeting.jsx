5046fcef4a3729468d53071803ea27deae19fe93
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-02-06 11:31:09 +0500

    learned more

--- 01_vite_react/src/greeting.jsx
+++ 01_vite_react/src/greeting.jsx
@@ -1,9 +1,10 @@
 function Greeting() {
 
+ let name = "Ammar"; 
     return (
-      <h1>Hello from developer Ammar</h1>
+      <h1>Hello from developer {name} </h1>
     )
   }
-  
+ 
   export default Greeting
   
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -3,7 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<App />
+function PtiSlogan ()
+{
+    return (
+        <h2>East or West Pti is Best!</h2>
+    )
+}
 
+const exampleElement = (
+    <a href="https://www.google.com" target='_blank'>Google</a>
 )
+
+// It will not work because .render() of React doesnot accept this syntax
+// const reactElement = 
+// {
+//     type: "a",
+//     props:{
+//         href: "https://www.google.com",
+//         target: "_main"
+//     },
+//     children:"Go to google"
+// }
+
+//We can create Element and pass this b/c react will accept it
+const AnotherReactElement = React.createElement(
+    'a',
+    {href:"https://www.google.com", target:"_main"},
+    "Go to google"
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+   <>
+   AnotherReactElement
+   <App/>
+   </>
+    )
