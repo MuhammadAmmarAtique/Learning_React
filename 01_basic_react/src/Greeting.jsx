49fbbca8755be0a531ee50cb55375ab4e9d7b489
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-02-09 16:59:47 +0500

    new files appeared after running eject command

--- 01_basic_react/src/Greeting.jsx
+++ 01_basic_react/src/Greeting.jsx
@@ -1,7 +1,11 @@
 function Greeting() {
 
     return(
+        <>
         <h1>Hello from developer Ammar</h1>
+        <p>I run npm run eject command so thats why this project may be differenet from others</p>
+        <p>Ya wali command tab use ki jati hai jab haam apni app ko react saay nikalna chahiyaa uar kisi aur framework mai use kar na chaah haaiy.</p>
+        </>
     )
     
 }
