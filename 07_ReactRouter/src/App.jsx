0e9c8f5308622fc98d56c95728ed3fe343f41b0a
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-02-21 11:38:34 +0500

    Created a new project for React Router

---
+++ 07_ReactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-red-100 text-4xl'> React Router Dom</h1>
+    </>
+  )
+}
+
+export default App
