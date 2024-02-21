dae65b77a7bbf4a62b0b896327468065ba399faa
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-02-21 23:41:13 +0500

    learned about routing

--- 07_ReactRouter/src/App.jsx
+++ 07_ReactRouter/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1 className='bg-red-100 text-4xl'> React Router Dom</h1>
+  
     </>
   )
 }
--- 07_ReactRouter/src/main.jsx
+++ 07_ReactRouter/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "About-Us",
+        element: <About/>
+      },
+      {
+        path: "Contact-Us",
+        element: <Contact/>
+      }
+    ]
+    
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
