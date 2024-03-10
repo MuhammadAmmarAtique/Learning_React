d1168e5181a97aa0ade2fc20194ec267342e55bd
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-10 15:37:55 +0500

    completed making reducer

--- 11_reduxToolkit/src/features/todo/todoSlice.js
+++ 11_reduxToolkit/src/features/todo/todoSlice.js
@@ -1,12 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: [{id: 1,text: "Hello world",completed: false}]
-}
+  todos: [{ id: 1, text: "Hello world" }],
+};
 
-const todoSlice = createSlice({
-    name:"todo",
-    initialState,
-    
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducer: {
 
-    })
+    //reducer 1
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+
+    //reducer 2
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const {addTodo,removeTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
--- 11_reduxToolkit/src/store/store.js
+++ 11_reduxToolkit/src/store/store.js
@@ -1,3 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit"
+import todoReducer from '../features/todo/todoSlice'; //importing todoSlice.reducer 
 
-export const store = configureStore({});
+
+
+export const store = configureStore({
+    reducer:todoReducer
+});
