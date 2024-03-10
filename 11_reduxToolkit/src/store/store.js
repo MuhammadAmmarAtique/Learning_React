68c15364e42c1a6ac1774a696cb7af374f61f2dd
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-10 21:51:38 +0500

    added comments

--- 11_reduxToolkit/src/store/store.js
+++ 11_reduxToolkit/src/store/store.js
@@ -1,8 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit"
 import todoReducer from '../features/todo/todoSlice'; //importing todoSlice.reducer 
 
-
-
 export const store = configureStore({
     reducer:todoReducer
 });
