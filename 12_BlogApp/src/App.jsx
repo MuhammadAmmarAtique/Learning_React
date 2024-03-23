26a106e903b5097141c2704d9c7c915940c06958
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-23 18:01:44 +0500

    runned tailwind successfully

--- 12_BlogApp/src/App.jsx
+++ 12_BlogApp/src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   // console.log(import.meta.env.VITE_APPWRITE_URL);
 
   return (
-   <h1>Blog app using Appwrite</h1>
+    <h1 className="text-3xl font-bold underline text-yellow-400">
+    Hello world!
+  </h1>
   )
 }
 
