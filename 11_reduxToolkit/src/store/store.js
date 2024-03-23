5d20aa898ec289a5e07750a0bc0bbea2b5fc8cf3
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-23 15:23:24 +0500

    improved naming

--- 11_reduxToolkit/src/store/store.js
+++ 11_reduxToolkit/src/store/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit"
-import todoReducer from '../features/todo/todoSlice'; //importing todoSlice.reducer 
+import todoSliceReducer from '../features/todo/todoSlice'; //importing todoSlice.reducer 
 
 export const store = configureStore({
-    reducer:todoReducer
+    reducer:todoSliceReducer
 });
