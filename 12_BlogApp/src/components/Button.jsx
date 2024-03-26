4cbcfbae4d7b4978ffd65cb0b9bf2426391fca66
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-03-26 14:42:13 +0500

    Designed reusable button component

---
+++ 12_BlogApp/src/components/Button.jsx
@@ -0,0 +1,20 @@
+function Button({
+  children, // text inside button, children is  just a fancy name for text programmers give usually
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  type = "button",
+  ...props // other properties like placeholder etc can also be given.
+}) {
+  return (
+    <button
+      className={` px-4 py-2  rounded-lg ${bgColor} ${textColor} ${className}`}
+      {...props}
+      {...type}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- 12_BlogApp/src/components/index.js
+++ 12_BlogApp/src/components/index.js
@@ -3,5 +3,6 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
+import Button from "./Button";
 
-export { Header, LogoutBtn, Footer, Container, Logo };
+export { Header, LogoutBtn, Footer, Container, Logo, Button };
