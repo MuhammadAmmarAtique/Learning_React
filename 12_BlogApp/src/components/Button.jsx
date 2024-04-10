08bee34300b0feb898b4542d98f8b309ae8435a2
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-04-10 22:31:20 +0500

    Fixed one more bug which was causing too much errors in console

--- 12_BlogApp/src/components/Button.jsx
+++ 12_BlogApp/src/components/Button.jsx
@@ -7,11 +7,7 @@ function Button({
   ...props // other properties like placeholder etc can also be given.
 }) {
   return (
-    <button
-      className={` px-4 py-2  rounded-lg ${bgColor} ${textColor} ${className}`}
-      {...props}
-      {...type}
-    >
+    <button type={type} className={` px-4 py-2  rounded-lg ${bgColor} ${textColor} ${className}`}{...props}>
       {children}
     </button>
   );
