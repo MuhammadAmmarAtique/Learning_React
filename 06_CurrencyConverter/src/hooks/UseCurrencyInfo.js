0f85e2546edb0ad007d9fac931eec0a14e92e599
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-16 20:59:01 +0500

    Added new working exchange api

--- 06_CurrencyConverter/src/hooks/UseCurrencyInfo.js
+++ 06_CurrencyConverter/src/hooks/UseCurrencyInfo.js
@@ -6,7 +6,7 @@ function UseCurrencyInfo(currency) {
 
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
       .then((resp) => resp.json())
       .then((resp) => setData(resp[currency]))
