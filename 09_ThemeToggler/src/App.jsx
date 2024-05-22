44999453f8fd68f549171fd6fc707d59bcdb6aae
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-22 21:25:37 +0500

    formatted files

--- 09_ThemeToggler/src/App.jsx
+++ 09_ThemeToggler/src/App.jsx
@@ -18,29 +18,28 @@ function App() {
   }
 
   //Actual change in theme
+  useEffect(() => {
 
-useEffect(()=>{
+    const htmlElem = document.querySelector('html');
+    htmlElem.classList.remove("light", "dark");
+    htmlElem.classList.add(themeMode)
 
-  const htmlElem = document.querySelector('html');
-  htmlElem.classList.remove("light","dark");
-  htmlElem.classList.add(themeMode)
-
-},[themeMode])
+  }, [themeMode])
 
   return (
-   <ThemeProvider value={{themeMode,darkMode,lightMode} }>
+    <ThemeProvider value={{ themeMode, darkMode, lightMode }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                <ThemeBtn/>
-            </div>
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
 
-            <div className="w-full max-w-sm mx-auto">
-                <Card/>                      
-            </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
-   </ThemeProvider>
+    </ThemeProvider>
   )
 }
 
