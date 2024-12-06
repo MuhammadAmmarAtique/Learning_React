c3e6cbcbd48904b6bcfa5f2910310504714746c8
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-12-06 23:02:31 +0500

    learned different concepts of react hook form

--- 16_ReactHookForm/src/App.jsx
+++ 16_ReactHookForm/src/App.jsx
@@ -1,12 +1,53 @@
-import './App.css'
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onsubmit = async (data) => {
+    //simulating/faking api response
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log(data);
+  };
 
   return (
-    <>
-   hello g
-    </>
-  )
+    <form onSubmit={handleSubmit(onsubmit)}>
+      <div>
+        <label>First Name</label>
+        <input
+          {...register("firstName", {
+            required: true,
+            minLength: { value: 3, message: "ml 3" },
+            maxLength: { value: 6, message: "ml 6" },
+          })}
+        />
+        {errors.firstName && (
+          <p className="error-msg">{errors.firstName.message}</p>
+        )}
+      </div>
+
+      <div>
+        <label>Middle Name</label>
+        <input {...register("middleName")} />
+      </div>
+
+      <div>
+        <label>Last Name</label>
+        <input {...register("lastName")} />
+      </div>
+
+      <input
+        type="submit"
+        disabled={isSubmitting}
+        value={isSubmitting ? "Submitting" : "Submit"}
+      />
+    </form>
+  );
 }
 
-export default App
+export default App;
